6c5f53b222294bd0ff3db9cd9438d3066797337b
DanielaSMIS917920 <smis917920@ugb.edu.sv>
2024-10-30 14:29:51 -0600

    Solucion a errores en install.db

--- install.php
+++ install.php
@@ -3,30 +3,29 @@
 include 'db.php';
 
 //Se establece la creación de las tablas para la base de datos despensa_feliz_db
-$sql = "
-CREATE TABLE IF NOT EXISTS productos (
+$sql = "CREATE TABLE IF NOT EXISTS productos (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     nombre_producto VARCHAR(255) NOT NULL,
     categoria VARCHAR(255) NOT NULL,
     proveedor VARCHAR(255) NOT NULL
 
-);
+)";
 
-CREATE TABLE IF NOT EXISTS inventario (
+$sql = "CREATE TABLE IF NOT EXISTS inventario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     producto_id INT NOT NULL,
     cantidad INT NOT NULL,
     fecha_actualizacion DATE NOT NULL,
     FOREIGN KEY (producto_id) REFERENCES productos(id)
-);
+)";
 
-CREATE TABLE IF NOT EXISTS facturacion (
+$sql = "CREATE TABLE IF NOT EXISTS facturacion (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE NOT NULL,
     total DECIMAL(10, 2) NOT NULL
-);
+)";
 
-CREATE TABLE IF NOT EXISTS detalles_factura (
+$sql = "CREATE TABLE IF NOT EXISTS detalles_factura (
     id INT AUTO_INCREMENT PRIMARY KEY,
     factura_id INT NOT NULL,
     producto_id INT NOT NULL,
@@ -34,7 +33,7 @@ CREATE TABLE IF NOT EXISTS detalles_factura (
     precio DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (factura_id) REFERENCES facturacion(id),
     FOREIGN KEY (producto_id) REFERENCES productos(id)
-);";
+)";
 
 
 //Se verifica si la creación a sido exitosa, sino, avisar que hubo un error
